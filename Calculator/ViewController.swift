8a880b959f0986c0fc66671f4d10fa03ef8c7747
Ilya Dombrovsky <61749139+ilyaDombrovsky@users.noreply.github.com>
2022-07-12 16:28:06 +0300

    start

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var stillTyping = false
+    var firstNum: Double = 0
+    var mathSign = false
+    
+    @IBAction func digits(_ sender: UIButton) {
+        
+        if mathSign == true {
+            result.text = String(sender.tag)
+            mathSign = false
+            
+        }
+        
+        let number = String(sender.tag)
+        
+        if number == "0" && result.text == "0" {
+            stillTyping = false
+        } else {
+            if stillTyping {
+                if result.text!.count < 9 {
+                    result.text! += number
+                }
+            } else {
+                result.text = number
+                stillTyping = true
+            }
+        }
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if result.text != "" {
+            if sender.tag == 11 {
+                
+            } else if sender.tag == 12 {
+                
+            } else if sender.tag == 13 {
+                
+            } else if sender.tag == 14 {
+                result.text = "+"
+            }
+        }
+    }
+    @IBOutlet weak var result: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
